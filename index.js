6f415ed12991f89ac46cf776a4c8231bc817d5d1
Rudiethenomad <111630606+Rudiethenomad@users.noreply.github.com>
2023-04-21 12:58:50 +0200

    break

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 
+
+
+
+
+
 // Caching HTML elements
 
-
-const porduct = document.getElementById('products')
+/*
+const porducts = document.getElementById('products')
 const brand = document.getElementById('brand');
 const image = document.getElementById('image');
 const title = document.getElementById('title');
@@ -13,17 +18,6 @@ const rating = document.getElementById('rating');
 const discountPercentage = document.getElementById('discount');
 
 
-function renderPhones(products){
-    products.forEach(products => {
-      
-        const div = document.getElementById('brand');
-const image = document.getElementById('image');
-const title = document.getElementById('title');
-const price = document.getElementById('price');
-const category = document.getElementById('category');
-const discription = document.getElementById('discription');
-const rating = document.getElementById('rating');
-const discountPercentage = document.getElementById('discount');
 
 
            title.innerText = `title: ${products.title}`
@@ -41,20 +35,50 @@ const discountPercentage = document.getElementById('discount');
            cardsContainer.appendChild(div)
 
 
-    });          
-      
-};
+function renderPhones(data){
+  products.then(response => response.json())
     
+  products.forEach(
+  function load (){
+    addEventListener(window.onload);
+      });
+  
+  const populatePhones = (products) => {
+    for (const products of products) {
+      const productCardHtml = `
+        <div class="row">
+        <div class="col-4">
+          <div class="products">
+          <div class="card">
+            <div class="card-body">
+              <div id="product">
+                
+              <img id="image"src="#">
+              <h2 id="product-title"></h2>
+              <h3 id="product-brand"></h3>
+              <h4 id="product-category"></h4>
+              <p id="product-description"></p>
+              <div id="product-price-container">
+                  <h3 id="product-price">$</h3>
+                 <h3 id="product-discountPercentage"></h3>
+              </div>
+              </div>
+          </div>
+          </div>
+          </div>
+`};
+    }
+  };
+  */
 
-
-
-const fetchData = async () => {
+const renderProducts = async () => {
 
   try {
 
     const response = await fetch('https://dummyjson.com/products');
 
     const data = await response.json();
+   //.then(renderPhones)
 
     console.log(data);
 
@@ -63,7 +87,6 @@ const fetchData = async () => {
     console.log(error);
 
   }
-
+  
 };
-
-fetchData()
+renderProducts()
