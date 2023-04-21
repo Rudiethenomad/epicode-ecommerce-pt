30aeee4e5d35d0d16405813bc9f66d514d6a69d0
Rudiethenomad <111630606+Rudiethenomad@users.noreply.github.com>
2023-04-21 15:22:19 +0200

    break

--- index.js
+++ index.js
@@ -4,89 +4,37 @@
 
 
 
-// Caching HTML elements
-
-/*
-const porducts = document.getElementById('products')
-const brand = document.getElementById('brand');
-const image = document.getElementById('image');
-const title = document.getElementById('title');
-const price = document.getElementById('price');
-const category = document.getElementById('category');
-const discription = document.getElementById('discription');
-const rating = document.getElementById('rating');
-const discountPercentage = document.getElementById('discount');
 
 
-
-
-           title.innerText = `title: ${products.title}`
-           price.innerText = `price: ${products.price}`
-           category.innerText = `category: ${products.price}`
-
-           div.appendChild(brand)
-           div.appendChild(image)
-           div.appendChild(title)
-           div.appendChild(price)
-           div.appendChild(category)
-           div.appendChild(discription)
-           div.appendChild(rating)
-           div.appendChild(discount)
-           cardsContainer.appendChild(div)
-
-
-function renderPhones(data){
-  products.then(response => response.json())
+     fetch('https://dummyjson.com/products')
+      .then(res => {
+      return  res.json();
+      })
     
-  products.forEach(
-  function load (){
-    addEventListener(window.onload);
-      });
-  
-  const populatePhones = (products) => {
-    for (const products of products) {
-      const productCardHtml = `
-        <div class="row">
-        <div class="col-4">
-          <div class="products">
-          <div class="card">
-            <div class="card-body">
-              <div id="product">
-                
-              <img id="image"src="#">
-              <h2 id="product-title"></h2>
-              <h3 id="product-brand"></h3>
-              <h4 id="product-category"></h4>
-              <p id="product-description"></p>
-              <div id="product-price-container">
-                  <h3 id="product-price">$</h3>
-                 <h3 id="product-discountPercentage"></h3>
-              </div>
-              </div>
+      .then(data => {
+        data.forEach(function(products){
+          const markup = `        
+          <div class="products">     
+          <img id="image"src="${products.image}">
+          <h2 id="product-title">${products.title}</h2>
+          <h3 id="product-brand">${products.brand}</h3>
+          <h4 id="product-category">${products.category}</h4>
+          <p id="product-description">${products.description}</p>
+          <div id="product-price-container">
+              <h3 id="product-price">$${products.price}</h3>
+             <h3 id="product-discountPercentage">${products.discount}</h3>
           </div>
           </div>
-          </div>
-`};
-    }
-  };
-  */
+      </div>
+      </div>
+      </div>
+       `
 
-const renderProducts = async () => {
+            document.querySelector('products').insertAdjacentHTML('beforeend', markup);
 
-  try {
+        });
 
-    const response = await fetch('https://dummyjson.com/products');
+      })
+      .catch(error => console.log (error));
 
-    const data = await response.json();
-   //.then(renderPhones)
-
-    console.log(data);
-
-  } catch (error) {
-
-    console.log(error);
-
-  }
-  
-};
-renderProducts()
+    
--- trash.js
+++ trash.js
@@ -76,3 +76,84 @@ document.addEventListener('DOMContentLoaded', function() {
       mainPage.innerHTML += bookCardHtml;
     }
     */
+
+
+    // Caching HTML elements
+
+/*
+const porducts = document.getElementById('products')
+const brand = document.getElementById('brand');
+const image = document.getElementById('image');
+const title = document.getElementById('title');
+const price = document.getElementById('price');
+const category = document.getElementById('category');
+const discription = document.getElementById('discription');
+const rating = document.getElementById('rating');
+const discountPercentage = document.getElementById('discount');
+
+
+
+
+           title.innerText = `title: ${products.title}`
+           price.innerText = `price: ${products.price}`
+           category.innerText = `category: ${products.price}`
+
+           div.appendChild(brand)
+           div.appendChild(image)
+           div.appendChild(title)
+           div.appendChild(price)
+           div.appendChild(category)
+           div.appendChild(discription)
+           div.appendChild(rating)
+           div.appendChild(discount)
+           cardsContainer.appendChild(div)
+
+
+function renderPhones(data){
+  products.then(response => response.json())
+    
+  products.forEach(
+  function load (){
+    addEventListener(window.onload);
+      });
+  
+  const populatePhones = (products) => {
+    for (const products of products) {
+      const productCardHtml = `
+        <div class="row">
+        <div class="col-4">
+          <div class="products">
+          <div class="card">
+            <div class="card-body">
+              <div id="product">
+                
+              <img id="image"src="#">
+              <h2 id="product-title"></h2>
+              <h3 id="product-brand"></h3>
+              <h4 id="product-category"></h4>
+              <p id="product-description"></p>
+              <div id="product-price-container">
+                  <h3 id="product-price">$</h3>
+                 <h3 id="product-discountPercentage"></h3>
+              </div>
+              </div>
+          </div>
+          </div>
+          </div>
+`};
+    }
+  };
+  */
+
+    /*
+  div.appendChild(brand)
+           div.appendChild(image)
+           div.appendChild(title)
+           div.appendChild(price)
+           div.appendChild(category)
+           div.appendChild(discription)
+           div.appendChild(rating)
+           div.appendChild(discount)
+           cardsContainer.appendChild(div)
+
+      */
