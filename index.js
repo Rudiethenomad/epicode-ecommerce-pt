7ccc4b09a340c1ec200a40426da273b80187a20a
Rudiethenomad <111630606+Rudiethenomad@users.noreply.github.com>
2023-04-21 16:22:06 +0200

    update

--- index.js
+++ index.js
@@ -10,12 +10,19 @@
       .then(res => {
       return  res.json();
       })
-    
-      .then(data => {
-        data.forEach(products => {
+    .then((object) => {
+      console.log(object);
+      console.log(typeof object);
+      return object;
+    })
+    .then ((object) => {
+      return object.products;
+    })
+      .then(products => {
+        products.forEach(products => {
           const markup = `        
           <div class="products">     
-          <img id="image"src="${products.image}">
+          <img id="image"src="${products.images[0]}">
           <h2 id="product-title">${products.title}</h2>
           <h3 id="product-brand">${products.brand}</h3>
           <h4 id="product-category">${products.category}</h4>
@@ -30,7 +37,7 @@
       </div>
        `
 
-            document.querySelector('products').insertAdjacentHTML('beforeend', markup);
+            document.querySelector('.products').insertAdjacentHTML('beforeend', markup);
 
         });
 
