19f0f0ff522c87f7289eb3e29552afd85bdd1971
Rudiethenomad <111630606+Rudiethenomad@users.noreply.github.com>
2023-04-28 09:12:03 +0200

    Finished

--- index.js
+++ index.js
@@ -23,6 +23,10 @@
           
           const markup = ` 
                  
+
+
+
+          
           <div class="products">    
           <img id="image"src="${products.images[0]}">
           <h2 id="product-title">${products.title}</h2>
@@ -42,7 +46,45 @@
       </div>
        `
      
-
+ $(function () {
+   
+        let goToCartIcon = function($addTocartBtn){
+          let $cartIcon = $(".my-cart-icon");
+          let $image = $('<img width="30px" height="30px" src="' + $addTocartBtn.data("image") + '"/>').css({"position": "fixed", "z-index": "999"});
+          $addTocartBtn.prepend($image);
+          let position = $cartIcon.position();
+          $image.animate({
+            top: position.top,
+            left: position.left
+          }, 500 , "linear", function() {
+            $image.remove();
+          });
+        }
+    
+        $('.my-cart-btn').myCart({
+          classCartIcon: 'my-cart-icon',
+          classCartBadge: 'my-cart-badge',
+          classProductQuantity: 'my-product-quantity',
+          classProductRemove: 'my-product-remove',
+          classCheckoutCart: 'my-cart-checkout',
+          affixCartIcon: true,
+          showCheckoutModal: true,
+          clickOnAddToCart: function($addTocart){
+            goToCartIcon($addTocart);
+          },
+          clickOnCartIcon: function($cartIcon, products, totalPrice, totalQuantity) {
+            console.log("cart icon clicked", $cartIcon, products, totalPrice, totalQuantity);
+          },
+          checkoutCart: function(products, totalPrice, totalQuantity) {
+            console.log("checking out", products, totalPrice, totalQuantity);
+          },
+          getDiscountPrice: function(products, totalPrice, totalQuantity) {
+            console.log("calculating discount", products, totalPrice, totalQuantity);
+            return totalPrice ;
+          }
+        });
+    
+      });
             document.querySelector('.products').insertAdjacentHTML('beforeend', markup);
 
         });
